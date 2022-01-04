2d63585a42d31671f0eb520e87d3b3fcedd12323
maxbarsukov <maximbarsukov@bk.ru>
2022-01-04 22:05:20 +0300

    feat: :sparkles: implement evaluation

--- lib/capriccio.rb
+++ lib/capriccio.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 require_relative 'capriccio/version'
+require_relative 'capriccio/runtime/object'
+require_relative 'capriccio/runtime/message'
+require_relative 'capriccio/runtime/method'
+require_relative 'capriccio/runtime/bootstrap'
 
 module CapriccIo
-  class Error < StandardError; end
-  # Your code goes here...
+  class Error < RuntimeError
+    attr_accessor :current_message
+
+    def message
+      super + " in message `#{@current_message}` at line #{@current_message.line}"
+    end
+  end
+
+  def self.eval(code)
+    message = Message.parse(code)
+    message.call(Runtime)
+  end
 end
