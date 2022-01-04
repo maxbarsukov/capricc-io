65e64f754d3242642cc8343766c9c1f9bf27add3
maxbarsukov <maximbarsukov@bk.ru>
2022-01-04 20:47:55 +0300

    feat: :sparkles: setup gem

---
+++ lib/capriccio.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'capriccio/version'
+
+module CapriccIo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/capriccio/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CapriccIo
+  VERSION = '0.1.0'
+end
